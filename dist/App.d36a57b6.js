5d0d30695db3c8fa76a2e51f1fd62baf373631a3
Nur Ichsan <n.ichank70@gmail.com>
2020-11-06 13:44:46 +0700

    perubahan saya

--- dist/App.d36a57b6.js
+++ dist/App.d36a57b6.js
@@ -68138,7 +68138,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59937" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50657" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
